8c36d9da51fc95b3b1286010b4d200963c6525ed
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-04-16 14:10:10 +0530

    Spring UC2-3: implemented controllers

--- 02 - Spring Intro/intro/src/main/java/com/intro/controllers/HelloRestController.java
+++ 02 - Spring Intro/intro/src/main/java/com/intro/controllers/HelloRestController.java
@@ -4,10 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorld {
-
-    @GetMapping("/")
-    public String helloWorld() {
-        return "Hello World";
+public class HelloRestController {
+    @GetMapping("/rest")
+    public String hello() {
+        return "hello world from rest controller";
     }
 }
