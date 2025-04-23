d9e34c6ab4ba2d5968634bbd2bf252c3208d22c0
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-04-23 10:11:13 +0530

    UC5: (payroll) added validations to employee DTO

--- 03 - Spring REST/springPayroll/src/main/java/com/springpayroll/dtos/EmployeeDTO.java
+++ 03 - Spring REST/springPayroll/src/main/java/com/springpayroll/dtos/EmployeeDTO.java
@@ -1,6 +1,9 @@
 package com.springpayroll.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,12 +16,17 @@ import java.sql.Date;
 public class EmployeeDTO implements Serializable {
     private Integer id;
 
-    @NotEmpty
+    @NotEmpty(message = "Name must not be empty")
     @Pattern(regexp = "^[A-Za-z ]{2,50}$", message = "Name must be 2-50 characters and contain only letters and spaces")
     private String name;
 
+    @NotEmpty(message = "Gender must not be empty")
+    @Pattern(regexp = "M | F", message = "Gender needs to be M or F")
     private Character gender;
 
+    @NotNull(message = "startDate must not be null")
+    @JsonFormat(pattern = "dd MM yyyy")
+    @PastOrPresent(message = "startDate should be past or present")
     private Date startDate;
 
     private PayrollDTO payroll;
